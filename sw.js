0c0a9c45d1a9cbec910b9e6a30a2ffb9eba02214
mewens95 <mewens95@hotmail.com>
2026-09-03 18:35:19 +0100

    Enable GardenPro app caching

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-const CACHE='gardenpro-connected-v1';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>self.clients.claim());
+const CACHE='gardenpro-app-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET') return;
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(
+    fetch(request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(request,copy));
+      return response;
+    }).catch(()=>caches.match(request).then(response=>response||caches.match('./index.html')))
+  );
+});

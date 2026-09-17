28301a9b2273823237aafe5057854ed1ae4c3a70
mewens95 <mewens95@hotmail.com>
2026-09-17 01:26:59 +0100

    Fix Android package ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.gardenpro.app',
+  appId: 'com.gardenpro.gardening',
   appName: 'GardenPro',
   webDir: 'www',
   bundledWebRuntime: false
